5a51a93e237d356540cb6e94d8b7b830ff1da843
fatkuns <fatkuns@cf05ef52-c1ec-11de-a250-c7f789e21d83>
2009-10-28 05:46:46 +0000

    初始化

---
+++ trunk/src/cn/gdpu/service/StudentService.java
@@ -0,0 +1,45 @@
+package cn.gdpu.service;
+
+import java.util.List;
+
+import cn.gdpu.vo.Student;
+
+public interface StudentService {  
+	  
+    /** 
+     * 保存学生信息 
+     * @param student 
+     */  
+    public abstract void add(Student student);  
+  
+    /** 
+     * 根据学号删除学生信息 
+     * @param studentId 
+     */  
+    public abstract void delete(Integer sno);  
+  
+    /** 
+     * 更新学生信息 
+     * @param student 
+     */  
+    public abstract void update(Student student);  
+  
+    /** 
+     * 根据学号获取学生信息 
+     * @param studentId 
+     */  
+    public abstract Student getStudent(Integer sno);  
+  
+    /** 
+     * 获取全部学生信息 
+     * @param studentId 
+     */  
+    public abstract List<Student> getStudents();  
+      
+    /** 
+     * 通过学生姓名获取学生信息 
+     * @param studentId 
+     */  
+    public abstract List<Student> getStudentsByName(String name);  
+  
+}  

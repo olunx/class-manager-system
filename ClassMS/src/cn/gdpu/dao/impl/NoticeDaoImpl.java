b1b8f3b04f8cb59cac84595ae8c760eabf3cc296
fatkuns <fatkuns@cf05ef52-c1ec-11de-a250-c7f789e21d83>
2009-10-29 09:12:57 +0000

    struts配置分文件

--- ClassMS/src/cn/gdpu/dao/impl/NoticeDaoImpl.java
+++ ClassMS/src/cn/gdpu/dao/impl/NoticeDaoImpl.java
@@ -10,28 +10,31 @@ import cn.gdpu.vo.Notice;
 public class NoticeDaoImpl extends HibernateDaoSupport implements NoticeDao {
 
 	public void deleteNoticeByID(int nid) {
-		// TODO Auto-generated method stub
-
+		Notice notice = null;
+		notice = queryNoticeByID(nid);
+		if (notice != null)
+			this.getHibernateTemplate().delete(notice);
 	}
 
 	public void insertNotice(Notice notice) {
 		this.getHibernateTemplate().save(notice);
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Notice> queryAllNotices() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Notice> notices = null;
+		notices = this.getHibernateTemplate().find("from Notice");
+		return notices;
 	}
 
 	public Notice queryNoticeByID(int id) {
 		Notice notice = null;
-		notice = (Notice)this.getHibernateTemplate().get(Notice.class, id);
+		notice = (Notice) this.getHibernateTemplate().get(Notice.class, id);
 		return notice;
 	}
 
 	public void updateNotice(Notice notice) {
-		// TODO Auto-generated method stub
-
+		this.getHibernateTemplate().update(notice);
 	}
 
 }

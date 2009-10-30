ca760c7019870173d9e63b51124893b4412ad7a7
fatkuns <fatkuns@cf05ef52-c1ec-11de-a250-c7f789e21d83>
2009-10-30 12:20:07 +0000

    修复jsp request乱码

--- ClassMS/src/cn/gdpu/action/NoticeAction.java
+++ ClassMS/src/cn/gdpu/action/NoticeAction.java
@@ -1,25 +1,34 @@
 package cn.gdpu.action;
 
 import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.RequestAware;
 
 import cn.gdpu.service.NoticeService;
 import cn.gdpu.vo.Notice;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class NoticeAction extends ActionSupport {
+public class NoticeAction extends ActionSupport implements RequestAware {
 	private static final long serialVersionUID = 1L;
 	private NoticeService noticeService;
+	private Map<String, Object> request;
 
 	public String doList() throws Exception {
 		@SuppressWarnings("unused")
 		List<Notice> notices = null;
 		notices = noticeService.getAllNotices();
+		request.put("notices", notices);
 		return SUCCESS;
 	}
 
 	public void setNoticeService(NoticeService noticeService) {
 		this.noticeService = noticeService;
+	}
+
+	public void setRequest(Map<String, Object> request) {
+		this.request = request;
 	}
 	
 	

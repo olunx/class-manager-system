2910123a5d0e7407118e105d6efda444e17bcf0f
fatkuns <fatkuns@cf05ef52-c1ec-11de-a250-c7f789e21d83>
2009-10-30 11:31:52 +0000

    添加JSTL包

--- trunk/ClassMS/src/cn/gdpu/action/NoticeAction.java
+++ trunk/ClassMS/src/cn/gdpu/action/NoticeAction.java
@@ -1,13 +1,25 @@
 package cn.gdpu.action;
 
+import java.util.List;
+
+import cn.gdpu.service.NoticeService;
+import cn.gdpu.vo.Notice;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class NoticeAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
+	private NoticeService noticeService;
 
 	public String doList() throws Exception {
-		
+		@SuppressWarnings("unused")
+		List<Notice> notices = null;
+		notices = noticeService.getAllNotices();
 		return SUCCESS;
+	}
+
+	public void setNoticeService(NoticeService noticeService) {
+		this.noticeService = noticeService;
 	}
 	
 	

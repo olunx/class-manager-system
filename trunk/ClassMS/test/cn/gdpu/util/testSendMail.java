f17fe3ba2935105051a39623d5aec7289d6793c2
fatkuns <fatkuns@cf05ef52-c1ec-11de-a250-c7f789e21d83>
2009-11-09 13:26:56 +0000

    加入javamail

---
+++ trunk/ClassMS/test/cn/gdpu/util/testSendMail.java
@@ -0,0 +1,28 @@
+package cn.gdpu.util;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+
+public class testSendMail extends TestCase {
+
+	public void testSendMail() {
+		try {
+			List<String> list = new ArrayList<String>();
+			list.add("fatkun8@gmail.com");
+			list.add("cyk.cn@qq.com");
+			list.add("olunx@qq.com");
+			list.add("fuchal@qq.com");
+			
+			MailSender.getInstance().sendMail("测试标题"+new Date(),	"这是一封测试邮件", list);
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}

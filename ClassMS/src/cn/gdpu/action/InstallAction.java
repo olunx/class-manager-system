4c6cea8eda0cf20be4a9258d28b0f04c7d7c8601
fatkuns <fatkuns@cf05ef52-c1ec-11de-a250-c7f789e21d83>
2009-11-12 02:34:20 +0000

    login.htm

---
+++ ClassMS/src/cn/gdpu/action/InstallAction.java
@@ -0,0 +1,26 @@
+package cn.gdpu.action;
+
+import cn.gdpu.service.AdminService;
+import cn.gdpu.vo.Admin;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class InstallAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+	private AdminService adminService;
+
+	@Override
+	public String execute() throws Exception {
+		Admin admin = new Admin();
+		admin.setUsername("admin");
+		admin.setPassword("admin");
+		admin.setAdminName("测试管理员");
+		adminService.add(admin);
+		return SUCCESS;
+	}
+
+	public void setAdminService(AdminService adminService) {
+		this.adminService = adminService;
+	}
+	
+}

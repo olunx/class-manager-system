f4263d524b61f8367a239f55dba83df4419ee9cd
fatkuns <fatkuns@cf05ef52-c1ec-11de-a250-c7f789e21d83>
2009-11-15 05:41:51 +0000

    加入日期选择JS(content/js/calendar)，内有Demo

---
+++ trunk/ClassMS/WebRoot/content/js/calendar/config.js
@@ -0,0 +1,12 @@
+﻿var langList = 
+[
+	{name:'en',	charset:'UTF-8'},
+	{name:'zh-cn',	charset:'gb2312'},
+	{name:'zh-tw',	charset:'GBK'}
+];
+
+var skinList = 
+[
+	{name:'blueFresh',	 charset:'gb2312'},
+	{name:'ext',	 charset:'gb2312'}
+];

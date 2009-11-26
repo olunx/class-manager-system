27a21b0a090a64c7ea721076eb5d75ca76efdb09
fatkuns <fatkuns@cf05ef52-c1ec-11de-a250-c7f789e21d83>
2009-11-26 14:39:04 +0000

    修改一些页面的dialog

--- ClassMS/WebRoot/content/js/duty.js
+++ ClassMS/WebRoot/content/js/duty.js
@@ -20,43 +20,43 @@
 		});
 	});
 
-$(function() {
-	
-	$("#dialog").dialog({
-		bgiframe: true,
-		autoOpen: false,
-		width:320,
-		height: 175,
-		modal: true
-	});
-	
-	$('#create').click(function() {
-		$('#dialog').dialog('open');
-	})
-	
-	$("a[rel='update']").click(function() {
-		
-		//alert($(this).attr('value'));
-		
-		$.get($(this).attr('value'), function(data){
-			  //alert("Data Loaded: " + data);
-			  $('#update-dialog').append(data);
-			  $('#update-dialog').dialog('open');
-		});
-		
-		
-		$("#update-dialog").dialog({
-			bgiframe: true,
-			autoOpen: false,
-			width:320,
-			height: 245,
-			modal: true,
-			close: function() {
-				$('#update-dialog').empty();
-			}
-		});
-	})
-	
-});
+//$(function() {
+//	
+//	$("#dialog").dialog({
+//		bgiframe: true,
+//		autoOpen: false,
+//		width:320,
+//		height: 175,
+//		modal: true
+//	});
+//	
+//	$('#create').click(function() {
+//		$('#dialog').dialog('open');
+//	})
+//	
+//	$("a[rel='update']").click(function() {
+//		
+//		//alert($(this).attr('value'));
+//		
+//		$.get($(this).attr('value'), function(data){
+//			  //alert("Data Loaded: " + data);
+//			  $('#update-dialog').append(data);
+//			  $('#update-dialog').dialog('open');
+//		});
+//		
+//		
+//		$("#update-dialog").dialog({
+//			bgiframe: true,
+//			autoOpen: false,
+//			width:320,
+//			height: 245,
+//			modal: true,
+//			close: function() {
+//				$('#update-dialog').empty();
+//			}
+//		});
+//	})
+//	
+//});
 
 
